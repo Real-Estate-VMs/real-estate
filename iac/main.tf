7b3190905c0897646aed92d28cc88b686b29fbae
Enzo Dorigon Leandrini <dorigonenzo@gmail.com>
2026-09-17 23:06:30 -0300

    Adjusted VMs

--- iac/main.tf
+++ iac/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     libvirt = {
-      source = "dmacvicar/libvirt"
+      source  = "dmacvicar/libvirt"
       version = "0.8.3"
     }
   }
@@ -11,65 +11,65 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-locals{
-  vm_count = 2
+locals {
+  vms = ["generator", "processing", "bigdata", "monitoring"]
 }
 
-resource "libvirt_volume" "ubuntu_base"{
-  name = "ubuntu-24.04-base.qcow2"
-  pool = "default"
+resource "libvirt_volume" "ubuntu_base" {
+  name   = "ubuntu-24.04-base.qcow2"
+  pool   = "default"
   source = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
   format = "qcow2"
 }
 
-resource "libvirt_volume" "vm_disk"{
-  count = local.vm_count
+resource "libvirt_volume" "vm_disk" {
+  count = length(local.vms)
 
-  name = "devops-${count.index + 1}.qcow2"
-  pool = "default"
+  name           = "${local.vms[count.index]}.qcow2"
+  pool           = "default"
   base_volume_id = libvirt_volume.ubuntu_base.id
-  format = "qcow2"
+  format         = "qcow2"
 }
 
-resource "libvirt_cloudinit_disk" "init"{
-  count = local.vm_count
+resource "libvirt_cloudinit_disk" "init" {
+  count = length(local.vms)
 
-  name = "devops-${count.index + 1}-cloudinit.iso"
+  name = "${local.vms[count.index]}-cloudinit.iso"
   pool = "default"
 
-  user_data = templatefile("${path.module}/cloud_init.cfg",{
-    hostname = "devops-${count.index + 1}"
-    ssh_key = trimspace(file(pathexpand("~/.ssh/devops_lab.pub")))
+  user_data = templatefile("${path.module}/cloud_init.cfg", {
+    hostname = local.vms[count.index]
+    ssh_key  = trimspace(file(pathexpand("~/.ssh/devops_lab.pub")))
   })
 }
 
-resource "libvirt_domain" "vm"{
-  count = local.vm_count
+resource "libvirt_domain" "vm" {
+  count = length(local.vms)
 
-  name = "devops-${count.index + 1}"
+  name   = local.vms[count.index]
   memory = 1024
-  vcpu = 1
+  vcpu   = 1
 
   cloudinit = libvirt_cloudinit_disk.init[count.index].id
 
   network_interface {
-    network_name = "default"
+    network_name   = "default"
     wait_for_lease = true
   }
 
-  disk{
+  disk {
     volume_id = libvirt_volume.vm_disk[count.index].id
   }
 
   console {
-    type = "pty"
+    type        = "pty"
     target_type = "serial"
     target_port = "0"
   }
 }
 
 output "ips" {
-    value = [
-      for vm in libvirt_domain.vm : vm.network_interface[0].addresses[0]
-    ]
+  value = {
+    for i, vm in libvirt_domain.vm : local.vms[i] => vm.network_interface[0].addresses[0]
   }
+}
